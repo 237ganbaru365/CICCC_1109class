eac490878e2c50f89c6b2f358015cf9d9db19c4e
{237ganbaru365} <{f.ganbaru365@gmail.com}>
2021-11-09 12:50:45 -0800

    first commit

---
+++ src/main/java/com/spring/pma/controller/HomeController.java
@@ -0,0 +1,35 @@
+package com.spring.pma.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.spring.pma.dao.iProjectRepository;
+import com.spring.pma.entity.Project;
+
+import com.spring.pma.dao.iEmployeeRepository;
+import com.spring.pma.entity.Employee;
+
+@Controller
+public class HomeController {
+	
+	@Autowired
+	iProjectRepository proRepo;
+
+	@Autowired
+	iEmployeeRepository empRepo;
+	
+	@GetMapping("/")
+	public String displayHome(Model model) {
+		List<Project> projects = proRepo.findAll();
+		model.addAttribute("projectList", projects);
+
+		List<Employee> employees = empRepo.findAll();
+		model.addAttribute("employeeList", employees);
+		return "main/home";
+	}
+
+}
